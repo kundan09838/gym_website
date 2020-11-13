c9546763c7cb879c2bc9c68c58db20c83eae822b
kundan09838 <74398155+kundan09838@users.noreply.github.com>
2020-11-13 19:03:51 +0530

    Add files via upload

---
+++ se.js
@@ -0,0 +1,25 @@
+
+
+
+    
+    var menu = document.getElementById("menu");
+    
+    var mbar = document.getElementById("item");
+    
+    item.style.right = '-300px';
+    
+    menu.onclick = function() {
+        
+        
+         if(item.style.right == '-300px')
+         {
+          item.style.right = '0';
+         }
+         else{
+          item.style.right = '-300px';
+         }
+         
+    }
+    
+ 
+
